b505f22a8bc75f2c0c490f4c9ffa0caa1d435df5
miqo <wartanyan.miqo@gmail.com>
2023-12-14 14:38:39 +0400

    Initial Commit

---
+++ Zakhrpha/ZakhrphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakhrphaApp.swift
+//  Zakhrpha
+//
+//  Created by miqo on 14.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ZakhrphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
